68c9aa4de39db75280b2dc53f47c7e6d454d39f8
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-01-25 00:21:51 -0300

    remove favicon.ico link from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,6 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang="en">
-			<head>
-				<link rel="icon" href="/favicon.ico" />
-			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
